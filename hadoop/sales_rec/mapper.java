a84e34d8713ec9e15b05370ced7c0848e47d9243
Abhishek Tangod <abhishekvtangod@pop-os.localdomain>
2021-11-22 16:37:57 +0530

    country total sales updated

--- hadoop/sales_rec/mapper.java
+++ hadoop/sales_rec/mapper.java
@@ -29,9 +29,18 @@ public class mapper extends Mapper<Object, Text, Text, IntWritable>{
         //     context.write(word, one);
         // }
         String[] line = value.toString().split(",");
+        if(line[0].equals("Transaction_date")){
+            return; //header of csv
+        }
+        // for(String val: line){
+        //     System.out.print(val + " | ");
+        // }
+        // System.out.println();
+
         String country = "_country_" + line[7];
         String payment_type = "_payment_type_" + line[3];
-        context.write(new Text(country), new IntWritable(1));
+        int price = Integer.parseInt(line[2]);
+        context.write(new Text(country), new IntWritable(price));
         context.write(new Text(payment_type), new IntWritable(1));
     }
 }
